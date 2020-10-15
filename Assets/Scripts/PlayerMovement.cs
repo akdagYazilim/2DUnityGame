aaf72f46456a14cdd772b2294d0b119618c483de
akdagYazilim <57890265+akdagYazilim@users.noreply.github.com>
2020-10-15 20:52:30 +0300

    print yazıldı
    print yazıldı

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("deneme");
     }
 
     // Update is called once per frame
